0c05bc8b2e4249ca4d4a2b7b00cc06081137c7d2
Eddie Chen <122704850+eddiemaximchen@users.noreply.github.com>
2023-01-19 10:09:42 +0800

    Create novelTxt.py

---
+++ novelTxt.py
@@ -0,0 +1,61 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import os
+
+def getNovel(articleNum):
+    
+    url='https://tw.hjwzw.com/Book/Chapter/%s'%articleNum
+    html = urlopen(url)
+    bs = BeautifulSoup(html.read(),'html.parser')
+    title=bs.find("head").title.get_text()
+    #求得書名的長度
+    length=title.find('/')  
+    #表達式無法過濾去掉非文字部份 所以先去掉
+    if '，'  in title:
+        title=title.replace('，','')
+        length=length-1
+
+    if '：'  in title:
+        title=title.replace('：','')
+        length=length-1
+
+    if '!'  in title:
+        title=title.replace('：','')
+        length=length-1        
+    #去掉書名後的文字 動態給表達式長度
+    regex01=r'\w{%s}(?=\/)'%length
+    title=re.match(regex01,title)
+    #內文標題是書名
+    str='<h1>%s</h1>'%title[0]
+    chapters=[]
+    #找到div的dom
+    books=bs.find('div',{'id':'tbchapterlist'})
+    #把網址改成絕對路徑
+    for a in books.select('a'):
+        a['href']="https://tw.hjwzw.com"+a['href']
+        #準備把所有章節變成文字檔
+        chapters.append(a['href'])
+    #找出所有章節
+    for book in books:
+        str=str+'%s'%book
+    for chapter in chapters:
+        link = urlopen(chapter)
+        text=BeautifulSoup(link.read(),'html.parser').get_text()
+
+    #放在子資料夾novels 如果沒有就新增    
+    folderPath = os.path.join(os.getcwd(), 'novels')
+    if not os.path.exists(folderPath):
+        os.makedirs(folderPath)
+    #寫入檔案 檔名是書名
+    path='./novels/%s.txt'%title[0]
+    fileobj =open(path, 'wt',encoding='UTF-8')
+    print(text,file=fileobj)
+    #寫入全文 
+    # path2='./novels/%s.txt'%title[0]
+    # with open(path2, "a") as file_object:
+    #     file_object.write(text)
+    fileobj.close()
+if __name__=="__main__":  
+#鬥破蒼穹
+    getNovel('1642')
